ecb6dc1e779150895ee91de062b603eb09c067a5
AYEOOON <k93853471@gmail.com>
2025-03-31 23:36:04 +0900

    feat: @PathVariable을 활용한 url 매핑 중복 제거

--- kotlin/src/main/kotlin/com/hi/kotlin/HtmlController.kt
+++ kotlin/src/main/kotlin/com/hi/kotlin/HtmlController.kt
@@ -3,6 +3,7 @@ package com.hi.kotlin;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable
 
 @Controller
 public class HtmlController {
@@ -12,14 +13,16 @@ public class HtmlController {
         return "index"
     }
 
-    @GetMapping("/sign")
-    fun signForm(model: Model) : String{
-        return "sign"
-    }
+    @GetMapping("/{formType}")
+    fun signForm(model: Model, @PathVariable formType:String) : String{
+        var response : String = ""
 
-    @GetMapping("/login")
-    fun loginForm(model: Model) : String{
-        return "login"
+        if(formType.equals("sign")){
+            response="sign"
+        }
+        else if(formType.equals("login")){
+            response="login"
+        }
+        return response
     }
-
 }
