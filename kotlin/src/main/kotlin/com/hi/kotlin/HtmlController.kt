8327a6b19e6fe5b50a2b0000331bc08127d6616e
AYEOOON <k93853471@gmail.com>
2025-03-31 23:17:21 +0900

    feat: Sign 화면 개발과 url 매핑

---
+++ kotlin/src/main/kotlin/com/hi/kotlin/HtmlController.kt
@@ -0,0 +1,19 @@
+package com.hi.kotlin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HtmlController {
+
+    @GetMapping("/")
+    fun index(model:Model):String{
+        return "index"
+    }
+
+    @GetMapping("/sign")
+    fun signForm(model: Model) : String{
+        return "sign"
+    }
+}
