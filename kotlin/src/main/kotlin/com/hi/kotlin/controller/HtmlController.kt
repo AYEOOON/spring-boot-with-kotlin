539cc48610d6eabf9683e7962741a53f27a591f7
AYEOOON <k93853471@gmail.com>
2025-04-04 16:06:40 +0900

    feat: 로그인 기능 구현

--- kotlin/src/main/kotlin/com/hi/kotlin/controller/HtmlController.kt
+++ kotlin/src/main/kotlin/com/hi/kotlin/controller/HtmlController.kt
@@ -2,6 +2,8 @@ package com.hi.kotlin.controller
 
 import com.hi.kotlin.entity.User
 import com.hi.kotlin.repository.Repository
+import com.sun.net.httpserver.HttpServer
+import jakarta.servlet.http.HttpSession
 import java.security.MessageDigest
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
@@ -48,13 +50,47 @@ class HtmlController {
                  @RequestParam(value="id") userId:String,
                  @RequestParam(value="password") password:String):String{
         try{
-            val cryptopass = crypto(password)
+            val cryptoPass = crypto(password)
 
-            val user = repository.save(User(userId, cryptopass))
+            val user = repository.save(User(userId, cryptoPass))
         }catch(e:Exception){
             e.printStackTrace()
         }
         model.addAttribute("title", "sign success")
         return "index"
+    }
+
+    @PostMapping("/login")
+    fun postLogin(
+            model: Model,
+            session: HttpSession,
+            @RequestParam("id") userId: String,
+            @RequestParam("password") password: String
+    ): String {
+        return try {
+            val cryptoPass = crypto(password)
+            val dbUser = repository.findByUserId(userId)
+
+            if (dbUser != null) {
+                val dbPass = dbUser.password
+
+                if (cryptoPass == dbPass) {
+                    session.setAttribute("userId", dbUser.userId)
+                    model.addAttribute("title", "welcome")
+                    model.addAttribute("userId", userId)
+                    "welcome"
+                } else {
+                    model.addAttribute("title", "login")
+                    "login"  // 비밀번호 불일치
+                }
+            } else {
+                model.addAttribute("title", "login")
+                "login"  // 사용자 없음
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            model.addAttribute("title", "login")
+            "login"  // 예외 발생 시도 로그인 페이지로
+        }
     }
 }
